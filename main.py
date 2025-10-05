dbb9a0b06e9d9d96234d406b8059aad47c524dad
Buket Özceylan <buketozceylan@outlook.com>
2025-10-05 18:52:57 +0300

    PDF created

---
+++ main.py
@@ -0,0 +1,9 @@
+#Coding exercise for learning PDF Generation, document to document 
+
+from fpdf import FPDF
+
+pdf = FPDF(orientation='P', format='A4') #Generate a file that is in A4 format and Portrait orientation (L for Lanscape)
+
+
+
+
