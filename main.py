433a649386bb343c4e5fc6cbb9ebdb70d99bd4ad
Buket Özceylan <buketozceylan@outlook.com>
2025-10-05 19:21:11 +0300

    PDF file created

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-#Coding exercise for learning PDF Generation, document to document 
+#Coding exercise for learning PDF Generation, document type to document type convertion
 
 from fpdf import FPDF
 
 pdf = FPDF(orientation='P', format='A4') #Generate a file that is in A4 format and Portrait orientation (L for Lanscape)
+pdf.add_page() #For adding page
+
+pdf.set_font('Arial', 'B', 16)
+pdf.cell(w=0, h=12, txt="Hello World!", align="L", ln=1, border=1)
+pdf.cell(w=0, h=12, txt="Hello World!", align="L", ln=1, border=1)
+
+pdf.output("test.pdf") #create the test.pdf file
 
 
 
